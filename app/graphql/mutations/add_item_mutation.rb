9fe971a9327395b2feff00a249f5138fda211814
Steven Torrence <storrence88@gmail.com>
2020-02-21 20:51:05 -0600

    Create add_item_mutation.rb

---
+++ app/graphql/mutations/add_item_mutation.rb
@@ -0,0 +1,29 @@
+module Mutations
+  class AddItemMutation < BaseMutation
+   argument :title, String, required: true
+   argument :description, String, required: false
+   argument :image_url, String, required: false
+
+   field :item, Types::ItemType, null: false
+   field :errors, [String], null: false
+
+    def resolve(title:, description: nil, image_url: nil)
+      if context[:current_user].nil?
+        raise GraphQL::ExecutionError, 'You need to authenticate to perform this action'
+      end
+
+      item = Item.new(
+        title: title,
+        description: description,
+        image_url: image_url,
+        user: context[:current_user]
+      )
+
+      if item.save
+        { item: item }
+      else
+        { errors: item.errors.full_messages }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :add_item_mutation, mutation: Mutations::AddItemMutation
     field :sign_in, mutation: Mutations::SignInMutation
   end
 end
